34582313a965845b57b2edc923addcdc486fe72f
log114 <1142159325@qq.com>
2025-10-31 17:50:47 +0800

    v4.4.0 1. 支持APP在线更新

--- app/src/main/java/com/example/yikupayloadexample/util/ApiService.kt
+++ app/src/main/java/com/example/yikupayloadexample/util/ApiService.kt
@@ -4,6 +4,6 @@ import retrofit2.Response
 import retrofit2.http.GET
 
 interface ApiService {
-    @GET("your-version-api-endpoint") // 替换为您的实际API地址
+    @GET("payloadAppUpdate/api/version") // 替换为您的实际API地址
     suspend fun checkVersion(): Response<VersionResponse>
 }
